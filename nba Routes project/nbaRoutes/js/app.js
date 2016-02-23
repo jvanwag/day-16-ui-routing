4e14a7f7a95100794d859fe840177d6ef7d659b2
Jon Van Wagenen <jvanwag08@gmail.com>
2016-02-23 11:38:20 -0700

    finished all but the home page

---
+++ nba Routes project/nbaRoutes/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('nbaRoutes', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    $httpProvider.interceptors.push('httpRequestInterceptor');
+
+    $stateProvider
+    .state('home', {
+      url: '/',
+      controller:'homeCtrl',
+      templateUrl:'js/home/homeTmpl.html'
+    })
+
+    .state('teams', {
+      url: '/teams/:team',
+      templateUrl: 'js/teams/teamTmpl.html',
+      controller: 'teamCtrl',
+      resolve: {
+        teamData: function(teamService, $stateParams){
+        return teamService.getTeamData($stateParams.team)
+      }
+    }
+    })
+    $urlRouterProvider
+    .otherwise('/');
+
+});
---
+++ nba Routes project/nbaRoutes/js/home/homeService.js
@@ -0,0 +1,19 @@
+var app = angular.module('nbaRoutes');
+
+app.service('homeService', function($http, $q, teamService){
+  this.getAllData = function(){
+    var deferred = $q.defer();
+    var promiseGroup = {
+      utahjazz: teamService.getTeamData('utahjazz'),
+      losangeleslakers: teamService.getTeamData('losangeleslakers'),
+      miamiheat: teamService.getTeamData('miamiheat')
+
+    }
+    $q.all(promiseGroup)
+    .then(function(result){
+      deferred.resolve(result);
+    })
+    return deferred.promise;
+  }
+
+});
